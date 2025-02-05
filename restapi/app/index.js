3a7825bd6d5a659567adfc9c0dfbf3b2678911df
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-02-05 11:02:05 -0300

    chore: change POST request byte limit

--- restapi/app/index.js
+++ restapi/app/index.js
@@ -6,8 +6,8 @@ const { body, validationResult } = require('express-validator');
 let app = express();
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
+app.use(bodyParser.json({limit: '50mb'}));
 consign().include('routes').include('utils').into(app);
 app.listen(4000, '127.0.0.1', () => {
 
